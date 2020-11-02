0a2cfe9186164c281728110385a18244e649aef3
Tran Anh Tuat <tuattrananh@gmail.com>
2020-11-02 17:03:50 +0700

    create post

--- cms/post/views.py
+++ cms/post/views.py
@@ -4,4 +4,13 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='login')
 def index(request):
 	context = {}
-	return render(request, 'account/home.html', context)
+	return render(request, 'post/index.html', context)
+
+@login_required(login_url='login')
+def createPost(request):
+	if request.method == 'POST':
+		title = request.POST.get('title')
+		content = request.POST.get('content')
+
+	context = {}
+	# return render(request, 'account/login.html', context)
