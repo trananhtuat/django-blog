1a44b6c3fc3eb77e5ce732a23f82b3d508fc22bf
Tran Anh Tuat <tuattrananh@gmail.com>
2020-11-03 17:13:41 +0700

    update
    update page css
    use generic view
    update postdetail
    update homepage

--- cms/post/urls.py
+++ cms/post/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
+from .views import (
+	PostListView,
+	PostDetailView,
+	PostCreateView
+)
 from . import views
 
 urlpatterns = [
-  path('', views.index, name='post'),
+  path('', PostListView.as_view(), name='post'),
+  path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+  path('post/new', PostCreateView.as_view(), name='post-create'),
 ]
--- cms/post/views.py
+++ cms/post/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Post
+
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 
 @login_required(login_url='login')
 def index(request):
-	context = {}
-	return render(request, 'post/index.html', context)
+	context = {
+        'posts': Post.objects.all().order_by('-date_created')
+    }
+	return render(request, 'post/main.html', context)
 
 @login_required(login_url='login')
 def createPost(request):
@@ -13,4 +24,20 @@ def createPost(request):
 		content = request.POST.get('content')
 
 	context = {}
-	# return render(request, 'account/login.html', context)
+	return render(request, 'account/index.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'post/main.html'
+    context_object_name = 'posts'
+    ordering = ['-date_created']
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post/detail.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'post/create.html'
+
